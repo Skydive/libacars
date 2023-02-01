6e78e8b9f0e15a55a0014f85ad366cc380d1fb06
Khalid Aleem <me@precess.io>
2023-02-01 20:48:30 +0100

    Fixed build install

--- default.nix
+++ default.nix
@@ -1,37 +1,33 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation {
-  name = "libacars";
-  src = ./.;
+let
+  version = "2";
+in
+  pkgs.stdenv.mkDerivation {
+    name = "libacars-${version}";
+    src = ./.;
 
-  enableParallelBuilding = true;
+    outputs = [ "out" ];
+    enableParallelBuilding = true;
 
-  outputs = [ "out" "dev" ];
+    # setupHooks = [
+      # ./move-lib64.sh
+    # ];
 
-  buildInputs = with pkgs; [ 
-    pkgconfig
-    cmake
-  ];
+    buildInputs = with pkgs; [ 
+      pkgconfig
+      cmake
+    ];
 
-  propagatedBuildInputs = with pkgs; [
-    zlib
-    libxml2
-  ];
+    propagatedBuildInputs = with pkgs; [
+      zlib
+      libxml2
+    ];
 
-  configurePhase = ''
-    mkdir -p build && cd build
-    cmake ../
-  '';
 
-  buildPhase = ''
-    make -j8
-  '';
-
-  installPhase = ''
-    mkdir -p $out/lib
-    cp libacars/libacars-2.so.2 $out/lib
-    cp libacars/libacars-2.so $out/lib
-    cp libacars/libacars-2.a $out/lib
-    mkdir -p $out/lib/pkgconfig
-  '';
-}
+    # preInstallPhases = preInstallPhases ++ ''
+      
+      # rm libacars/libacars-2.pc
+      # rm $out/lib/pkgconfig/
+    # '';
+  }
